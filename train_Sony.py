d9bfbbcda5961c1ae9f49d0d86ac769fb17b7fe5
chengxinlong <chengxinlong@megvii.com>
2022-12-24 08:32:57 +0800

    SID_pytorch

--- train_Sony.py
+++ train_Sony.py
@@ -24,6 +24,7 @@ checkpoint_dir=os.path.join(train_parameter['output_prefix'],'result_Sony')
 result_dir=os.path.join(train_parameter['output_prefix'],'result_Sony')
 
 train_fns=glob.glob(os.path.join(gt_dir,'0*.ARW'))
+
 train_ids=[int(os.path.basename(train_fn)[0:5]) for train_fn in train_fns]
 
 ps=train_parameter['patch_size']
@@ -113,93 +114,82 @@ optimizer=torch.optim.Adam(lr=train_parameter['learning_rate'],params=net_model.
 
 # Raw data takes long time to load.keep them in memory after loaded.
 gt_images=[None]*6000
-input_image={}
-input_image['300']=[None]*len(train_ids)
-input_image['250']=[None]*len(train_ids)
-input_image['100']=[None]*len(train_ids)
+
 
 g_loss=np.zeros((5000,1))
 
 for epoch in range(train_parameter['start_epoch'],train_parameter['num_epoches']):
     cnt=0
-    try:
-        for ind in np.random.permutation(len(train_ids)-1):
-            train_id=train_ids[ind]
-            in_files=glob.glob(os.path.join(input_dir,'%05d_00*.ARW'%train_id))
-            in_path=in_files[np.random.randint(0,len(in_files))]
-            in_fn=os.path.basename(in_path)
-            
-            gt_files=glob.glob(os.path.join(gt_dir,'%05d_00*.ARW'%train_id))
-            gt_path=gt_files[0]
-            gt_fn=os.path.basename(gt_path)
-            in_exposure=float(in_fn[9:-5])
-            gt_exposure=float(gt_fn[9:-5])
-            ratio=min(gt_exposure/in_exposure,300)
-            
-            st=time.time()
-            cnt+=1
-            
-            if input_image[str(ratio)[0:3]][ind] is None:
-                try:
-                    raw=rawpy.imread(in_path)
-                except:
-                    print(in_path)
-                input_image[str(ratio)[0:3]][ind]=np.expand_dims(pack_raw(raw),axis=0)*ratio
-                try:
-                    gt_raw=rawpy.imread(gt_path)
-                except:
-                    print(gt_path)
-                im=gt_raw.postprocess(use_camera_wb=True,half_size=False,no_auto_bright=True,output_bps=16)
-                gt_images[ind]=np.expand_dims(np.float32(im/65535),axis=0)
-            
-            H=input_image[str(ratio)[0:3]][ind].shape[1]
-            W=input_image[str(ratio)[0:3]][ind].shape[2]
-            
-            xx=np.random.randint(0,W-ps)
-            yy=np.random.randint(0,H-ps)
-            input_patch=input_image[str(ratio)[0:3]][ind][:,yy:yy+ps,xx:xx+ps,:]
-            gt_patch=gt_images[ind][:,yy*2:yy*2+ps*2,xx*2:xx*2+ps*2,:]
-            
-            if np.random.randint(2,size=1)[0]==1:
-                input_patch=np.flip(input_patch,axis=1)
-                gt_patch=np.flip(gt_patch,axis=1)
-                
-            if np.random.randint(2,size=1)[0]==1:
-                input_patch=np.flip(input_patch,axis=2)
-                gt_patch=np.flip(gt_patch,axis=2)
-            
-            if np.random.randint(2,size=1)[0]==1:
-                input_patch=np.transpose(input_patch,(0,2,1,3))
-                gt_patch=np.transpose(gt_patch,(0,2,1,3))
-                
-            input_patch=np.minimum(input_patch,1.0)
-            input_patch=torch.Tensor(np.transpose(input_patch,[0,3,1,2])).to(device)
-            output=net_model(input_patch) 
-            output=output.permute([0,2,3,1])
-            output=torch.clamp(output,min=0.0,max=1.0)
-            G_current=G_loss(output,torch.tensor(gt_patch.copy()).to(device))
-            G_current.backward()
-            
-            optimizer.step()
-            optimizer.zero_grad()
-            g_loss[ind]=G_current.detach().cpu().numpy()
-            
-            print("%d %d Loss=%.3f Time=%.3f"%(epoch,cnt,np.mean(g_loss[np.where(g_loss)]),time.time()-st))
-            
-            if epoch%save_freq==0:
-                if not os.path.isdir(os.path.join(result_dir,'%04d'%epoch)):
-                    os.makedirs(os.path.join(result_dir,'%04d'%epoch))
-                
-                temp = np.concatenate((gt_patch[0, :, :, :], output.detach().cpu().numpy()[0, :, :, :]), axis=1)
-            
-                misc.toimage(temp * 255, high=255, low=0, cmin=0, cmax=255).save(
-                os.path.join(result_dir, '%04d/%05d_00_train_%d.jpg' % (epoch, train_id, ratio)))
-                torch.save(net_model.state_dict(), os.path.join(checkpoint_dir, 'model_%04d.pt' % epoch))
-    except:
-        logging.error(epoch)
-                
+    
+    for ind in np.random.permutation(len(train_ids)):
+
+        train_id=train_ids[ind]
+        in_files=glob.glob(os.path.join(input_dir,'%05d_00*.ARW'%train_id))
+        in_path=in_files[np.random.randint(0,len(in_files))]
+        in_fn=os.path.basename(in_path)
+        
+        gt_files=glob.glob(os.path.join(gt_dir,'%05d_00*.ARW'%train_id))
+        gt_path=gt_files[0]
+        gt_fn=os.path.basename(gt_path)
+        in_exposure=float(in_fn[9:-5])
+        gt_exposure=float(gt_fn[9:-5])
+        ratio=min(gt_exposure/in_exposure,300)
+        
+        st=time.time()
+        cnt+=1
         
         
+        input_image=np.expand_dims(pack_raw(rawpy.imread(in_path)),axis=0)*ratio
+        H=input_image.shape[1]
+        W=input_image.shape[2]
+        
+        xx=np.random.randint(0,W-ps)
+        yy=np.random.randint(0,H-ps)
+        input_patch=input_image[:,yy:yy+ps,xx:xx+ps,:]
+        
+        gt_raw=rawpy.imread(gt_path)
+        im=gt_raw.postprocess(use_camera_wb=True,half_size=False,no_auto_bright=True,output_bps=16)
+        im=np.expand_dims(np.float32(im/65535),axis=0)
+        gt_patch=im[:,yy*2:yy*2+ps*2,xx*2:xx*2+ps*2,:]
+        
+        if np.random.randint(2,size=1)[0]==1:
+            input_patch=np.flip(input_patch,axis=1)
+            gt_patch=np.flip(gt_patch,axis=1)
             
+        if np.random.randint(2,size=1)[0]==1:
+            input_patch=np.flip(input_patch,axis=2)
+            gt_patch=np.flip(gt_patch,axis=2)
         
+        if np.random.randint(2,size=1)[0]==1:
+            input_patch=np.transpose(input_patch,(0,2,1,3))
+            gt_patch=np.transpose(gt_patch,(0,2,1,3))
+            
+        input_patch=np.minimum(input_patch,1.0)
+        input_patch=torch.Tensor(np.transpose(input_patch,[0,3,1,2])).to(device)
+        output=net_model(input_patch) 
+        output=output.permute([0,2,3,1])
+        output=torch.clamp(output,min=0.0,max=1.0)
+        G_current=G_loss(output,torch.tensor(gt_patch.copy()).to(device))
+        G_current.backward()
         
+        optimizer.step()
+        optimizer.zero_grad()
+        g_loss[ind]=G_current.detach().cpu().numpy()
+        
+        print("%d %d Loss=%.3f Time=%.3f Filename=%s"%(epoch,cnt,np.mean(g_loss[np.where(g_loss)]),time.time()-st,gt_path))
+        
+        if epoch%save_freq==0:
+            if not os.path.isdir(os.path.join(result_dir,'%04d'%epoch)):
+                os.makedirs(os.path.join(result_dir,'%04d'%epoch))
+            
+            temp = np.concatenate((gt_patch[0, :, :, :], output.detach().cpu().numpy()[0, :, :, :]), axis=1)
+        
+            misc.toimage(temp * 255, high=255, low=0, cmin=0, cmax=255).save(
+            os.path.join(result_dir, '%04d/%05d_00_train_%d.jpg' % (epoch, train_id, ratio)))
+            torch.save(net_model.state_dict(), os.path.join(checkpoint_dir, 'model_%04d.pt' % epoch))
+            
+    
+    
+        
+    
+    
